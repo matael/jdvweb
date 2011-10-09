959a2377e161d7ae7f73f134b30803f1ad8ed811
Mathieu (matael) Gaborit <mat.gaborit@gmail.com>
2011-10-09 23:03:09 +0200

    Minor change to fit with new naming standard and new size of matrix

--- create_matricefile.py
+++ create_matricefile.py
@@ -1,10 +1,10 @@
 # Initialize the matrix file with a empty matrix
 
-number_of_rows = 50
+number_of_rows = 60
 number_of_cols = 60
 
 
-file = open("matrice.jdv",'w')
+file = open("Matrice.JDV",'w')
 for i in range(number_of_rows):
     for j in range(number_of_cols):
         file.write('0')
